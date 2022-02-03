5b3734789ba63aad6861a17be7040e865301adf0
Tim Hedstrom <hedstromt@gmail.com>
2022-02-03 15:51:34 -0800

    Lecture 2 - Added custom character class, basic movement, and class assets

---
+++ Source/ActionRoguelike/Private/TCharacter.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ATCharacter::ATCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+
+}
+
+// Called when the game starts or when spawned
+void ATCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ATCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+// Called every frame
+void ATCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("TurnLeft", this, &APawn::AddControllerYawInput);
+}
+
