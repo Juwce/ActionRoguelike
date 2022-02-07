d5faf4f22afe3f9446fae4ea5d7ba113694af07e
Tim Hedstrom <hedstromt@gmail.com>
2022-02-07 11:23:20 -0800

    exploding barrel in C++ (without using radial force COMPONENT)

---
+++ Source/ActionRoguelike/Private/TExplosiveBarrel.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TExplosiveBarrel.h"
+
+#include "TMagicProjectile.h"
+
+// Sets default values
+ATExplosiveBarrel::ATExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComp");
+
+	StaticMeshComp->SetSimulatePhysics(true);
+	StaticMeshComp->SetCollisionProfileName("PhysicsActor");
+
+	ImpulseFalloff = ERadialImpulseFalloff::RIF_Constant;
+	ImpulseStrength = 1000.f;
+	ImpulseRadius = 100.f;
+	
+	SetRootComponent(StaticMeshComp);
+
+}
+
+// Called when the game starts or when spawned
+void ATExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATExplosiveBarrel::Explode()
+{
+	const FVector Origin = StaticMeshComp->GetComponentLocation();
+	StaticMeshComp->AddRadialImpulse(Origin, ImpulseRadius, ImpulseStrength, ImpulseFalloff);
+}
+
+void ATExplosiveBarrel::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp,
+                                  bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	Explode();
+	UE_LOG(LogTemp, Warning, TEXT("Boom %s"), *Other->GetName());
+}
+
+// Called every frame
+void ATExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ActionRoguelike/Public/TExplosiveBarrel.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TExplosiveBarrel.generated.h"
+
+
+UCLASS()
+class ACTIONROGUELIKE_API ATExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+	
+	void Explode();
+
+	ERadialImpulseFalloff ImpulseFalloff;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMeshComp;
+
+	UPROPERTY(EditAnywhere)
+	float ImpulseStrength;
+	
+	UPROPERTY(EditAnywhere)
+	float ImpulseRadius;
+
+};
