463540d16bb6114221e007d3a87ee1ddda2ae382
Tim Hedstrom <hedstromt@gmail.com>
2022-02-22 15:19:21 -0800

    Fixed MagicProjectile destroying itself on overlap with player

--- Source/ActionRoguelike/Private/TMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/TMagicProjectile.cpp
@@ -29,7 +29,7 @@ void ATMagicProjectile::PostInitializeComponents()
 void ATMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                               UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		UTAttributeComponent* AttributeComp = Cast<UTAttributeComponent>(
 			OtherActor->GetComponentByClass(UTAttributeComponent::StaticClass())
